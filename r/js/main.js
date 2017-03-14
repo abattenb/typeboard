1d86396581cfceceb46d35695d67292659c3332b
Andrew Battenburg <abattenb@gmail.com>
2017-03-14 14:03:29 -0700

    Added 'random' color function, started adding google font auto complete, added connection to Google API

--- r/js/main.js
+++ r/js/main.js
@@ -6,7 +6,7 @@
 window.addEventListener('load', function() {
     //Start Typeboard
     Typeboard.init();
-    
+
 
     //
     // Init Eventlisteners
@@ -52,9 +52,15 @@ window.addEventListener('load', function() {
         Typeboard.addCharMapColumn();
     }, false);
 
+    //Random color theme
+   document.getElementById('random').addEventListener('click', function(){
+       Typeboard.randomColors();
+    }, false);
+
     //Clear Settings
    document.getElementById('clearSettings').addEventListener('click', function(){
        Typeboard.clearSettings();
     }, false);
+
 
 });
--- r/js/typeboard.js
+++ r/js/typeboard.js
@@ -5,8 +5,16 @@
  * So far IE11 is out.
  */
 
-//TODO: Redo clearing of settings
-//TODO: Random color function
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//TODO: RESTRICT GOOGLE API KEY BEFORE SHARING
+//TODO: RESTRICT GOOGLE API KEY BEFORE SHARING
+//TODO: RESTRICT GOOGLE API KEY BEFORE SHARING
+//TODO: RESTRICT GOOGLE API KEY BEFORE SHARING
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+//TODO: Redo the way cookies are handled (path, expire)
+//TODO: Save random colors into palette
 //TODO: Letter Spacing adjustment
 //TODO: Line-height adjustment
 //TODO: Header + paragraph
@@ -17,6 +25,8 @@ Typeboard = (function () {
 
 
 //Typeboard Variables
+
+    var key = 'AIzaSyBbtvvx9DFYu8-CbOHPTaBOdQVT-P_zg9Q';
     var typeColumnArray = [];
     var settings = [];
     var init_settings = {
@@ -27,6 +37,7 @@ Typeboard = (function () {
         "sampleText" : "Sphinx of black quartz, judge my vow.",
         "numberColumns" : 3 //Going to be the typeface id array
     };
+    var gFontsList = [];
 
 
 
@@ -38,6 +49,7 @@ Typeboard = (function () {
         });
     }
 
+    //TODO: Just fetch once
     //Adds a new Type Column to the right
     var addCharMapColumn = function() {
         var xhttp = new XMLHttpRequest();
@@ -79,7 +91,10 @@ Typeboard = (function () {
 
     //Clears settings cookie
     var clearSettings = function() {
+        //clear cookie
         document.cookie = '';
+        //clear random color settings
+        document.getElementsByTagName('body')[0].style = '';
         loadSettings();
     }
 
@@ -174,6 +189,33 @@ Typeboard = (function () {
 
     }
 
+    //TODO: Implement better color random function
+    //TODO: Add 'unwind' function to get previous random
+    //Casts the three main colors with a random hex
+    //DANGEROUS, YOU MAY NEVER SEE THOSE COLORS AGAIN!!!
+    //http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
+    var randomColors = function(){
+        var textColor = "#" + Math.random().toString(16).slice(-6).toString();
+        var backgroundColor =  "#" + Math.random().toString(16).slice(-6).toString();
+        var accentColor = "#" + Math.random().toString(16).slice(-6).toString();
+        document.getElementsByTagName('body')[0].style.setProperty('--text-color', textColor, null);
+        document.getElementsByTagName('body')[0].style.setProperty('--background-color', backgroundColor, null);
+        document.getElementsByTagName('body')[0].style.setProperty('--accent-color', accentColor, null);
+    }
+
+
+    var loadGFontsList = function(){
+        var url = "https://www.googleapis.com/webfonts/v1/webfonts?key=" + key;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                gFontsList = JSON.parse(xhttp.responseText).items;
+            }
+        }
+        xhttp.open("GET", url, true);
+        xhttp.send();
+
+    }
 
 
 
@@ -182,11 +224,36 @@ Typeboard = (function () {
     var init = function () {
         //Loads settings from cookie
         loadSettings();
+        loadGFontsList();
+
         addCharMapColumn();
         addCharMapColumn();
         addCharMapColumn();
 
+
+        new autoComplete({
+            selector: 'input[id="gTypeDropdown"]',
+            minChars: 1,
+            source: function(term, suggest){
+                term = term.toLowerCase();
+                var choices = gFontsList;
+                var matches = [];
+                for (var i = 0; i<choices.length; i++)
+                    if (~choices[i].family.toLowerCase().indexOf(term)) matches.push(choices[i].family);
+                suggest(matches);
+            }
+        });
+
+        setTimeout(function() {
+
+
+
+            console.log(gFontsList.length + " Google fonts loaded.")
+        }, 1000);
+
+
         console.log("Typeboard init successful!");
+
     };
 
 
@@ -200,7 +267,8 @@ Typeboard = (function () {
         updateTheme:updateTheme,
         toggleItalics:toggleItalics,
         addCharMapColumn:addCharMapColumn,
-        clearSettings:clearSettings
+        clearSettings:clearSettings,
+        randomColors:randomColors
 
     };
 }());
