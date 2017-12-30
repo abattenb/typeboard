213e02155af8d7d70d27eead43a2de8263fff9bb
Andrew Battenburg <abattenb@gmail.com>
2017-12-30 09:53:18 -0800

    Add SCSS support and refactors

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 
-import '@/assets/styles/styles.css';
+import '@/assets/styles/styles.scss';
 
 import App from './App';
 
